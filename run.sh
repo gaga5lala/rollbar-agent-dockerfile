cef6a5121b97c7e659e5229806a72b2074fbcca1
gaga5lala <gaga5lala1013@gmail.com>
2016-11-14 22:57:34 +0800

    Replace template env when docker run

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+config='/etc/rollbar-agent.conf'
+
+sed -i  "s/{ACCESS_TOKEN}/$ACCESS_TOKEN/g" $config
+sed -i  "s,{TARGETS},$TARGETS," $config
+
 /etc/init.d/rollbar-agent start
 tail -f /dev/null
